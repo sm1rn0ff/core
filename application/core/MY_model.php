cde235d892bb9beb34e52979d46a16ac00b624d2
sm1rn0ff <garichard01@gmail.com>
2013-11-18 04:31:21 +0100

    Ajout de MY_Model.php dans le folder core

--- application/core/MY_model.php
+++ application/core/MY_model.php
@@ -4,7 +4,7 @@
 
     class MY_Model extends CI_Model
     {
-        public function create( $options_echapees = array(), $options_non_echapees = array() )
+        public function create($options_echapees = array(), $options_non_echapees = array())
         {
             if( empty($options_echapees) && empty($options_non_echapees) )
             {
@@ -16,51 +16,54 @@
                                    ->insert($this->table);
         }
 
-        public function read()
+        
+        public function read($select = '*', $where = array(), $nb = null, $debut = null)
         {
-            public function read($select = '*', $where = array(), $nb = null, $debut = null)
+            return $this->db->select($select)
+                                ->from($this->table)
+                                ->where($where)
+                                ->limit($nb, $debut)
+                                ->get()
+                                ->result();
+        }
+        
+        public function update($where, $options_echappees = array(), $options_non_echappees = array())
+        {       
+            if(empty($options_echappees) AND empty($options_non_echappees))
             {
-                return $this->db->select($select)
-                                    ->from($this->table)
-                                    ->where($where)
-                                    ->limit($nb, $debut)
-                                    ->get()
-                                    ->result();
+                return false;
             }
-        }
 
-        public function update()
-        {
-            public function update($where, $options_echappees = array(), $options_non_echappees = array())
-            {       
-                //  Vérification des données à mettre à jour
-                if(empty($options_echappees) AND empty($options_non_echappees))
-                {
-                    return false;
-                }
-                
-                //  Raccourci dans le cas où on sélectionne l'id
-                if(is_integer($where))
-                {
-                    $where = array('id' => $where);
-                }
-
-                return (bool) $this->db->set($options_echappees)
-                                           ->set($options_non_echappees, null, false)
-                                           ->where($where)
-                                           ->update($this->table);
+            if(is_integer($where))
+            {
+                $where = array('id' => $where);
             }
+
+            return (bool) $this->db->set($options_echappees)
+                                       ->set($options_non_echappees, null, false)
+                                       ->where($where)
+                                       ->update($this->table);
+        }
+        
+
+        public function delete($where)
+        {   
+            if(is_integer($where))
+            {
+                $where = array('id' => $where);
+            }
+            
+            return (bool) $this->db
+                               ->where($where)
+                               ->delete($this->table);
         }
 
-        public function delete()
+        public function count($champ = array(), $valeur = null)
         {
-
-        }
-
-        public function count()
-        {
-            return $$this->db->where()
+            return $$this->db->where($champ, $valeur)
                              ->from($this->table)
                              ->count_all_result();
         }
     }
+
+    
